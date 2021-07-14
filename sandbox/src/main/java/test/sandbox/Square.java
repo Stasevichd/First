60476d34cac34322bff00ea69b19e2ae10a43a3e
Stasevichd <dg.stasevich@gmail.com>
2021-07-14 19:25:32 +0300

    Добавлен класс Point. Добавлены библиотека Testng

---
+++ sandbox/src/main/java/test/sandbox/Square.java
@@ -0,0 +1,12 @@
+package test.sandbox;
+
+public class Square {
+  public double l;
+
+  public Square(double l) {
+  this.l = l;
+  }
+  public  double area (){
+    return this.l * this.l;
+  }
+}
